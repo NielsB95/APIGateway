2c8f6ceccd18eded4288c4a1821502001734e43a
Niels Boerkamp <n.boerkamp@outlook.com>
2018-11-24 12:10:17 +0100

    Added the objects for microservice registeration

---
+++ APIGateway.Services/Entities/Endpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace APIGateway.Services.Entities
+{
+    public class Endpoint
+    {
+        public string Method { get; set; }
+        public string Pathname { get; set; }
+    }
+}
---
+++ APIGateway.Services/Entities/Microservice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIGateway.Services.Entities
+{
+    public class Microservice
+    {
+        public string Name { get; set; }
+        public Uri DomainName { get; set; }
+        public int Port { get; set; }
+
+        public IList<Endpoint> Endpoints { get; set; }
+    }
+}
