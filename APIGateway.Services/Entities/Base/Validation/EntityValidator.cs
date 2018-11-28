20f25b103355358553a36afd3c3a4f4e127658d6
Niels Boerkamp <n.boerkamp@outlook.com>
2018-11-28 22:55:08 +0100

    Added comments to the validation function

--- APIGateway.Services/Entities/Base/Validation/EntityValidator.cs
+++ APIGateway.Services/Entities/Base/Validation/EntityValidator.cs
@@ -7,6 +7,12 @@ namespace APIGateway.Services.Entities.Base.Validation
 {
     public static class EntityValidator
     {
+        /// <summary>
+        /// Validate the specified entity.
+        /// </summary>
+        /// <returns>The validate.</returns>
+        /// <param name="entity">Entity.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static bool Validate<T>(T entity) where T : IValidatable
         {
             // Get all properties of the current object.
