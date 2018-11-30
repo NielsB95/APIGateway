bbd3a0d404484f265cc96b83c03b15ebd45e2028
Niels Boerkamp <n.boerkamp@outlook.com>
2018-11-30 22:19:37 +0100

    Initialized the APIGateway.Queue project

---
+++ APIGateway.Queue/Register.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APIGateway.Queue
+{
+    public static class Register
+    {
+        public static IServiceCollection AddRequestQueue(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
