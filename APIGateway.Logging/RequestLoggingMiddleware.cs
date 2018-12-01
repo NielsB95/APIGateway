c16f1e0bbbdd619bc9ac12b108d91a90f41f8653
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-01 14:39:56 +0100

    Added logging to a .log file

--- APIGateway.Logging/RequestLoggingMiddleware.cs
+++ APIGateway.Logging/RequestLoggingMiddleware.cs
@@ -1,15 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace APIGateway.Logging
 {
     public class RequestLoggingMiddleware
     {
-        private readonly RequestDelegate _next;
+        private readonly RequestDelegate next;
+        private readonly string file = Path.Combine(Directory.GetCurrentDirectory(), "Log.log");
 
         public RequestLoggingMiddleware(RequestDelegate next)
         {
-            _next = next;
+            this.next = next;
         }
 
         public async Task Invoke(HttpContext context)
@@ -18,9 +21,22 @@ namespace APIGateway.Logging
 
             // Do something before
 
-            await _next(context);
+            await next(context);
 
-            // Maybe also something afterwards?
+            // Write the request to a file.
+            using (StreamWriter writer = File.AppendText(file))
+            {
+                // Compose the log.
+                var log = string.Format("{0} {1}\t{2}{3}", DateTime.Now, context.Request.Method, context.Request.Host, context.Request.Path);
+
+                // Append the query if there is one.
+                var query = context.Request.QueryString.ToString();
+                if (!String.IsNullOrEmpty(query))
+                    log = string.Format("{0}{1}", log, query);
+
+                // Write to 
+                writer.WriteLine(log);
+            }
         }
     }
 }
