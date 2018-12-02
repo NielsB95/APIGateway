2e07a053275e0905b15832c72f8219fedaca11d3
Niels Boerkamp <n.boerkamp@outlook.com>
2018-12-02 14:10:01 +0100

    Added unit tests for the various URL composers

---
+++ APIGateway.Services.Tests/EndpointTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using APIGateway.Services.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIGateway.Services.Tests
+{
+    [TestClass]
+    public class EndpointTests
+    {
+        [TestMethod]
+        public void EndpointSignatureWithoutMicroserviceTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate"
+            };
+
+            Assert.AreEqual("GET-Authenticate", endpoint.Signature);
+        }
+
+        [TestMethod]
+        public void EndpointSignatureWithMicroserviceTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate",
+                Microservice = new Microservice()
+                {
+                    Prefix = "Authentication"
+                }
+            };
+
+            Assert.AreEqual("GET-Authentication/Authenticate", endpoint.Signature);
+        }
+
+        [TestMethod]
+        public void EndpointGatewayUrlTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate",
+                Microservice = new Microservice()
+                {
+                    Prefix = "Authentication"
+                }
+            };
+
+            Assert.IsTrue(endpoint.GatewayUrl.EndsWith("Authentication/Authenticate", StringComparison.Ordinal));
+        }
+
+        [TestMethod]
+        public void EndpointServiceUrlTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate",
+                Microservice = new Microservice()
+                {
+                    Prefix = "Authentication",
+                    DomainName = "http://localhost",
+                    Port = 80
+                }
+            };
+
+            Assert.AreEqual("http://localhost:80/Authenticate", endpoint.ServiceUrl);
+        }
+
+        [TestMethod]
+        public void EndpointGatewayUrlWithoutMicroserviceTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate"
+            };
+
+            Assert.AreEqual("Authenticate", endpoint.GatewayUrl);
+        }
+
+        [TestMethod]
+        public void EndpointServiceUrlWithoutMicroserviceTest()
+        {
+            var endpoint = new Endpoint()
+            {
+                Method = "GET",
+                Pathname = "Authenticate"
+            };
+
+            Assert.AreEqual("Authenticate", endpoint.ServiceUrl);
+        }
+    }
+}
---
+++ APIGateway.Services.Tests/MicroserviceTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using APIGateway.Services.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APIGateway.Services.Tests
+{
+    [TestClass]
+    public class MicroserviceTests
+    {
+        [TestMethod]
+        public void ServiceUrlWithoutPortTest()
+        {
+            var service = new Microservice()
+            {
+                DomainName = "http://localhost"
+            };
+
+            Assert.AreEqual("http://localhost", service.ServiceUrl);
+        }
+
+        [TestMethod]
+        public void ServiceUrlWithPortTest()
+        {
+            var service = new Microservice()
+            {
+                DomainName = "http://localhost",
+                Port = 80
+            };
+
+            Assert.AreEqual("http://localhost:80", service.ServiceUrl);
+        }
+
+        [TestMethod]
+        public void GatewayBaseUrlTest()
+        {
+            var service = new Microservice()
+            {
+                Prefix = "Authentication"
+            };
+
+            Assert.IsTrue(service.GatewayBaseUrl.EndsWith("Authentication", StringComparison.Ordinal));
+        }
+    }
+}
